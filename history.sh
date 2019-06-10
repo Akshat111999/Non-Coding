538e53e841fb09cae1ff4413e9afac671a5d805e
Akshat <42962482+Akshat111999@users.noreply.github.com>
2019-06-10 21:43:03 +0530

    History command task completed
    Here we get to know how to check the history size, how to change it and save it permanently. Then to show the history with date and time. How to write in the .bash_history file and update the history in the terminal

---
+++ history.sh
@@ -0,0 +1,24 @@
+# to check history size
+echo $HISTSIZE
+# to change history size
+HISTSIZE=1999
+# save it permanent 
+vim .bashrc   # write HISTSIZE=1999 and save it
+# to show history with date and time
+HISTTIMEFORMAT="%d/%m/%y %T
+# update history entered manually in .bash_history file
+vim /home/lw/.bash_history  # write a command eg:- date
+
+
+
+
+
+
+
+
+
+
+
+
+
+
