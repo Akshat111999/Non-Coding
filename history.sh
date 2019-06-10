a7cc02bdc4af659cbd0f8f2d49ec3511ef509e56
Akshat <42962482+Akshat111999@users.noreply.github.com>
2019-06-10 21:45:22 +0530

    Update history.sh

--- history.sh
+++ history.sh
@@ -7,8 +7,7 @@ vim .bashrc   # write HISTSIZE=1999 and save it
 # to show history with date and time
 HISTTIMEFORMAT="%d/%m/%y %T
 # update history entered manually in .bash_history file
-vim /home/lw/.bash_history  # write a command eg:- date
-
+vim /home/lw/.bash_history  # write a command for eg:- date
 
 
 
